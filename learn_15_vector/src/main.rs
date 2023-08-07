ca2a4154c9b25c4dc45a6a37fc74bd7f07e90020
mochenya <xiaochen0517@qq.com>
2023-08-07 13:06:10 +0800

    vector

--- learn_15_vector/src/main.rs
+++ learn_15_vector/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     vector_use_new();
     vector_use_macro();
     vector_read();
+    vector_read_option_option();
+    vector_dy_element();
 }
 
 fn vector_use_new() {
@@ -27,4 +29,35 @@ fn vector_read() {
     v.push(40);
     let element_2: &i32 = v.get(3).expect("获取到的值为空");
     println!("third: {}", element_2);
+}
+
+fn vector_read_option_option() {
+    println!("====== vector_read_option_option ======");
+    let mut v: Vec<Option<i32>> = Vec::new();
+    v.push(Some(1));
+    v.push(Some(2));
+    v.push(Some(3));
+    v.push(None);
+    println!("v: {:?}", v);
+    let element_1: &Option<i32> = v.get(2).expect("获取到的值为空");
+    println!("third: {:?}", element_1);
+    let element_2: &Option<i32> = v.get(3).expect("获取到的值为空");
+    println!("third: {:?}", element_2);
+}
+
+fn vector_dy_element() {
+    println!("====== vector_dy_element ======");
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+    println!("row: {:?}", row);
 }

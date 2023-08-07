8439042ef3e1455a4127bc1a859f0822fa8d1758
mochenya <xiaochen0517@qq.com>
2023-08-07 08:53:34 +0800

    update mod name

--- learn_13_module/src/main.rs
+++ learn_13_module/src/main.rs
@@ -6,29 +6,29 @@ fn main() {
     let str = String::from("Hello world!");
     let first_word = str_util::get_first_word(&str);
     println!("first_word: {}", first_word);
-    A::a_func();
+    a::a_func();
 }
 
-mod A {
-    use crate::B;
+mod a {
+    use crate::b;
 
     pub fn a_func() {
         println!("a_func");
-        B::b_func();
+        b::b_func();
     }
 
-    pub mod A_A {
+    pub mod a_a {
         pub fn a_a_func() {
             println!("a_a_func");
         }
     }
 }
 
-mod B {
-    use crate::A;
+mod b {
+    use crate::a;
 
     pub(crate) fn b_func() {
         println!("b_func");
-        A::A_A::a_a_func();
+        a::a_a::a_a_func();
     }
 }

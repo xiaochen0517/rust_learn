9ac7e69a4ab9949fb9c4d9c602e49d674a876981
mochenya <xiaochen0517@qq.com>
2023-08-07 12:46:06 +0800

    vector

--- learn_15_vector/src/main.rs
+++ learn_15_vector/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     vector_use_new();
     vector_use_macro();
+    vector_read();
 }
 
 fn vector_use_new() {
@@ -16,4 +17,14 @@ fn vector_use_macro() {
     println!("====== vector_use_macro ======");
     let v = vec![10, 20, 30];
     println!("v: {:?}", v);
+}
+
+fn vector_read() {
+    println!("====== vector_read ======");
+    let mut v = vec![10, 20, 30];
+    let element_1: &i32 = v.get(2).expect("获取到的值为空");
+    println!("third: {}", element_1);
+    v.push(40);
+    let element_2: &i32 = v.get(3).expect("获取到的值为空");
+    println!("third: {}", element_2);
 }

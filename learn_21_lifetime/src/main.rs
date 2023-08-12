913079d7da7084f0acae73f4f41d2a1c979a82fa
mochenya <2827075398@qq.com>
2023-08-12 21:40:25 +0800

    lifetime

--- learn_21_lifetime/src/main.rs
+++ learn_21_lifetime/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     test_lifetime();
+    none_used_lifetime();
+    test_struct_lifetime();
+    test_struct_method();
 }
 
 fn test_lifetime() {
@@ -15,5 +18,62 @@ fn test_function<'a>(str1: &'a str, str2: &'a str) -> &'a str {
         str1
     } else {
         str2
+    }
+}
+
+fn none_used_lifetime() {
+    println!("====== none_used_lifetime ======");
+    let str1: String = String::from("abcd");
+    let result: &str;
+    {
+        let str2: String = String::from("xyz");
+        result = none_used_function(&str1, &str2);
+    }
+    println!("The longest string is {}", result);
+}
+
+fn none_used_function<'a>(str1: &'a str, str2: &str) -> &'a str {
+    println!("str1: {}", str1);
+    println!("str2: {}", str2);
+    str1
+}
+
+fn test_struct_lifetime() {
+    println!("====== test_struct_lifetime ======");
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let important_excerpt = ImportantExcerpt {
+        part: first_sentence,
+    };
+    println!("important : {:?}", important_excerpt);
+}
+
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn test_struct_method() {
+    println!("====== test_struct_method ======");
+    let struct_instance = MyStruct {
+        name: String::from("zhangsan"),
+        age: 18,
+    };
+    let name;
+    {
+        let test_var = 2u8;
+        name = struct_instance.get_name(&test_var);
+    }
+    println!("name: {}", name);
+}
+
+struct MyStruct {
+    name: String,
+    age: u32,
+}
+
+impl MyStruct {
+    fn get_name<'a>(&'a self, num: &u8) -> &'a str {
+        &self.name
     }
 }

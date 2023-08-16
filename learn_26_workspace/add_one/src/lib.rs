2177750837e224885c1550465f95949ed841e156
mochenya <xiaochen0517@qq.com>
2023-08-16 08:56:45 +0800

    workspace

--- learn_26_workspace/add_one/src/lib.rs
+++ learn_26_workspace/add_one/src/lib.rs
@@ -1,4 +1,4 @@
-use rand;
+pub use rand;
 
 pub fn add_one(x: i32) -> i32 {
     x + 1
--- learn_26_workspace/adder/src/main.rs
+++ learn_26_workspace/adder/src/main.rs
@@ -1,8 +1,11 @@
 use add_one;
+use add_one::rand;
 
 fn main() {
     let num = 10;
     println!("Hello, world! {} plus one is {}!", num, add_one::add_one(num));
     let rand_num = add_one::get_rand_num();
     println!("Random number: {}", rand_num);
+    let adder_rand_num = rand::random::<i32>();
+    println!("Adder random number: {}", adder_rand_num);
 }

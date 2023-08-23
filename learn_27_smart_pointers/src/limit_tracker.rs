085869101306da634a43cdb7a2d931524738217d
mochenya <2827075398@qq.com>
2023-08-23 20:56:39 +0800

    smart pointer

--- learn_27_smart_pointers/src/limit_tracker.rs
+++ learn_27_smart_pointers/src/limit_tracker.rs
@@ -21,40 +21,43 @@ impl<'a, T> LimitTracker<'a, T>
 
     pub fn set_value(&mut self, value: usize) {
         self.value = value;
-
         let percentage_of_max = self.value as f64 / self.max as f64;
-
-        if percentage_of_max >= 1.0 {
-            self.messager.send("Error: You are over your quota!");
-        } else if percentage_of_max >= 0.9 {
+        println!("max: {}, value: {}, percentage_of_max: {}", self.max, self.value, percentage_of_max);
+        if percentage_of_max > 1.0 {
+            self.messager.send("错误：当前参数已经超过最大值!");
+        } else if percentage_of_max < 0.9 && percentage_of_max >= 0.75 {
             self.messager
-                .send("Urgent warning: You've used up over 90% of your quota!");
-        } else if percentage_of_max >= 0.75 {
+                .send("警告: 当前剩余空间已经不足90%!");
+        } else if percentage_of_max < 0.75 && percentage_of_max >= 0.2 {
             self.messager
-                .send("Warning: You've used up over 75% of your quota!");
+                .send("警告: 当前剩余空间已经不足75%!");
+        } else if percentage_of_max < 0.2 {
+            self.messager
+                .send("警告: 当前剩余空间已经不足20%!");
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::cell::RefCell;
     use super::*;
 
     struct MockMessenger {
-        sent_messages: Vec<String>,
+        sent_messages: RefCell<Vec<String>>,
     }
 
     impl MockMessenger {
         fn new() -> MockMessenger {
             MockMessenger {
-                sent_messages: vec![],
+                sent_messages: RefCell::new(vec![String::from("test")]),
             }
         }
     }
 
     impl Messager for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.push(String::from(message));
+            self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
 
@@ -62,9 +65,19 @@ mod tests {
     fn it_sends_an_over_75_percent_warning_message() {
         let mock_messenger = MockMessenger::new();
         let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
-
         limit_tracker.set_value(80);
+        println!("mock_messenger.sent_messages: {:?}", mock_messenger.sent_messages.borrow());
+        limit_tracker.set_value(50);
+        println!("mock_messenger.sent_messages: {:?}", mock_messenger.sent_messages.borrow());
+        limit_tracker.set_value(10);
+        println!("mock_messenger.sent_messages: {:?}", mock_messenger.sent_messages.borrow());
+    }
 
-        assert_eq!(mock_messenger.sent_messages.len(), 1);
+    #[test]
+    fn it_sends_an_over_90_percent_warning_message() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+        limit_tracker.set_value(95);
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
     }
 }
--- learn_27_smart_pointers/src/main.rs
+++ learn_27_smart_pointers/src/main.rs
@@ -13,6 +13,7 @@ fn main() {
     my_box::test_my_box_drop();
     my_box::test_manually_drop();
     rc_list::test_basic_test_rc();
+    test_mutiple_ownership();
 }
 
 fn change_str(str: &str) {
@@ -39,4 +40,58 @@ fn test_box_var_compare() {
     let b = Box::new(5);
     assert_eq!(a, *b);
     println!("a = {}, b = {}", a, b);
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn test_mutiple_ownership() {
+    println!("====== test_mutiple_ownership ======");
+    let value = Rc::new(RefCell::new(5));
+    let value1 = Rc::new(RefCell::new(123));
+    let a = Rc::new(Cons(Rc::clone(&value),
+                         Rc::new(Cons(Rc::clone(&value1),
+                                      Rc::new(Nil)))));
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    println!("------ no change ------");
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+
+    println!("------ change ------");
+    *value.borrow_mut() += 10;
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+
+    println!("------ change again ------");
+    if let List::Cons(_, ref next) = *a {
+        if let List::Cons(_, ref next2) = **next {
+            let new_list = Rc::new(List::Cons(Rc::clone(&value), Rc::clone(next2)));
+        }
+    }
+
+    match *a {
+        Cons(ref value, ref next) => {
+            match *next.clone() {
+                Cons(ref value1, ref next1) => {
+                    *value1.borrow_mut() += 10;
+                }
+                Nil => println!("End of list ..."),
+            }
+        }
+        Nil => println!("End of list"),
+    }
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }

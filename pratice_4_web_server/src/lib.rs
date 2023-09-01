d02f57a37877bc6f9d8e73453d17f1023e6898bc
mochenya <2827075398@qq.com>
2023-09-01 21:15:02 +0800

    pratice

---
+++ pratice_4_web_server/src/lib.rs
@@ -0,0 +1,62 @@
+use std::sync::{Arc, mpsc, Mutex};
+use std::thread;
+
+pub struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {} got a job; executing.", id);
+            job();
+        });
+        Worker { id, thread: Some(thread) }
+    }
+}
+
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, _f: F)
+        where
+            F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(_f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+            println!("Worker {} shut down.", worker.id);
+        }
+    }
+}
--- pratice_4_web_server/src/main.rs
+++ pratice_4_web_server/src/main.rs
@@ -1,3 +1,43 @@
+use std::{fs, thread};
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use pratice_4_web_server::ThreadPool;
+
+// 监听地址
+static LISTEN_ADDRESS: &str = "127.0.0.1";
+// 监听端口
+static LISTEN_PORT: u32 = 7878u32;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(format!("{}:{}", LISTEN_ADDRESS, LISTEN_PORT)).unwrap();
+    println!("监听地址 http://{}:{}", LISTEN_ADDRESS, LISTEN_PORT);
+    let pool = ThreadPool::new(4);
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buffer_reader = BufReader::new(&mut stream);
+    let request_line = buffer_reader.lines().next().unwrap().unwrap();
+    println!("http_request: {:#?}", request_line);
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let html_content = fs::read_to_string(filename).unwrap();
+    let length = html_content.len();
+    let response = format!("{}\r\nContent-Length: {}\r\n\r\n{}", status_line, length, html_content);
+    stream.write_all(response.as_bytes()).unwrap();
 }
